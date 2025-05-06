8413ea442a7cd462a1b432e1401057a6ea04078f
Marcus <johnmarcus.loyola@letran.edu.ph>
2025-05-06 23:44:37 +0800

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace q2_q3_s5_loyola.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
